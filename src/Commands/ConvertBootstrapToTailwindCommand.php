74e5e7ebc67aef3f0a458fbe3a4893a9b3774849
Zakarialabib <Zakarialabib@users.noreply.github.com>
2022-12-16 23:04:48 +0000

    Fix styling

--- src/Commands/ConvertBootstrapToTailwindCommand.php
+++ src/Commands/ConvertBootstrapToTailwindCommand.php
@@ -3,14 +3,15 @@
 namespace Zakarialabib\ConvertBootstrapToTailwind\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Zakarialabib\ConvertBootstrapToTailwind\ConvertBootstrapToTailwind;
-use Illuminate\Support\Facades\File;
 
 class ConvertBootstrapToTailwindCommand extends Command
 {
     protected $signature = 'analyze:views {--change}';
+
     protected $description = 'Analyze view files and suggest Bootstrap to Tailwind CSS class name changes';
 
     public function handle(InputInterface $input, OutputInterface $output)
@@ -41,13 +42,13 @@ class ConvertBootstrapToTailwindCommand extends Command
      */
     protected function getViewFiles()
     {
-        return glob(base_path('resources/views') . '/*.blade.php');
+        return glob(base_path('resources/views').'/*.blade.php');
     }
 
     /**
      * Get the HTML from a file.
      *
-     * @param string $filePath
+     * @param  string  $filePath
      * @return string
      */
     protected function getHtmlFromFile(string $filePath)
@@ -58,8 +59,8 @@ class ConvertBootstrapToTailwindCommand extends Command
     /**
      * Write HTML to a file.
      *
-     * @param string $filePath
-     * @param string $html
+     * @param  string  $filePath
+     * @param  string  $html
      * @return void
      */
     protected function writeHtmlToFile(string $filePath, string $html)
